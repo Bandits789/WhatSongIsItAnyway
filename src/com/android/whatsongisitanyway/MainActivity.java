b3e15a96150a9b96d8880c0da1f48d8b95359e52
Olga Shestopalova <olgash@mit.edu>
2014-03-06 18:50:12 -0500

    RANDOM SONG PLAYING WORKS

--- src/com/android/whatsongisitanyway/MainActivity.java
+++ src/com/android/whatsongisitanyway/MainActivity.java
@@ -1,7 +1,5 @@
 package com.android.whatsongisitanyway;
 
-import java.io.FileDescriptor;
-
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -37,7 +35,7 @@ public class MainActivity extends Activity {
 	 */
 	public void play(View view) {
 		if (mediaPlayer == null) {
-			startPlayer(view);
+			startPlayer();
 		} else {
 			goToNextSong();
 		}
@@ -47,40 +45,23 @@ public class MainActivity extends Activity {
 	 * Starts up the mediaPlayer and starts playing the first song (if not
 	 * stopped or skipped, will keep playing songs until done)
 	 * 
-	 * @param view
 	 */
-	public void startPlayer(View view) {
+	public void startPlayer() {
 		game = new Game();
-		TextView textView = (TextView) findViewById(R.id.title);
 
 		// we can only skip from now on
 		Button button = (Button) findViewById(R.id.playButton);
 		button.setText("Skip");
 
-		// start first song
-		Music firstSong = game.getNextSong();
-		textView.setText(firstSong.getID());
-		mediaPlayer = MediaPlayer.create(view.getContext(), firstSong.getID());
-
-		// make sure rest of songs play
-		mediaPlayer
-				.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-					@Override
-					public void onCompletion(MediaPlayer mediaPlayer) {
-						goToNextSong();
-					}
-				});
-
-		// actually start!
-		mediaPlayer.start();
+		goToNextSong();
 	}
 
 	/**
 	 * Set the music player to go to the next song, if there are any left.
 	 * Release the player if we're done.
 	 * 
-	 * @param mediaPlayer
-	 *            the music player in question
+	 * @param context
+	 *            the context of the view
 	 */
 	public void goToNextSong() {
 		Music nextSong = game.getNextSong();
@@ -91,16 +72,26 @@ public class MainActivity extends Activity {
 			try {
 				// set the new title
 				textView.setText(nextSong.getID());
-				mediaPlayer.stop();
-				mediaPlayer.reset();
 
-				// get the music file
-				 FileDescriptor fd = getResources().openRawResourceFd(
-						nextSong.getID()).getFileDescriptor();
-				mediaPlayer.setDataSource(fd);
+				// stop old music player
+				if (mediaPlayer != null) {
+					mediaPlayer.stop();
+				}
 
-				// play it!
-				mediaPlayer.prepare();
+				// create new music player
+				mediaPlayer = MediaPlayer.create(textView.getContext(),
+						nextSong.getID());
+
+				// make sure rest of songs play
+				mediaPlayer
+						.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+							@Override
+							public void onCompletion(MediaPlayer mediaPlayer) {
+								goToNextSong();
+							}
+						});
+
+				// actually start!
 				mediaPlayer.start();
 
 			} catch (Exception e) {
@@ -110,9 +101,8 @@ public class MainActivity extends Activity {
 			// we're done!
 			// TODO: do something to alert user here...
 			mediaPlayer.release();
-
-			// so user can't skip
 			mediaPlayer = null;
+
 			Button button = (Button) findViewById(R.id.playButton);
 			button.setText("Play");
 			textView.setText("What Song Is It Anyway?");
