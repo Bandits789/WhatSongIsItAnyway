7591d28e1f9408391c482ebc8f9d041155841543
Annie Jiao <anniej@mit.edu>
2014-03-05 20:48:58 -0500

    wrote some shit for adt

--- src/com/android/whatsongisitanyway/Music.java
+++ src/com/android/whatsongisitanyway/Music.java
@@ -1,5 +1,7 @@
 package com.android.whatsongisitanyway;
 
+import android.R.integer;
+
 /**
  * Represents a music object.
  * 
@@ -9,9 +11,16 @@ package com.android.whatsongisitanyway;
 public class Music {
 	private int id;
 	private String name;
+	private int playCount;
+	private int timesCorrect;
+	private float avgGuessTime;
 
-	public Music(int id) {
+	public Music(int id, String name) {
 		this.id = id;
+		this.name = name;
+		this.playCount = playCount;
+		this.timesCorrect = timesCorrect;
+		this.avgGuessTime = avgGuessTime;
 	}
 
 	/**
@@ -22,5 +31,39 @@ public class Music {
 	public int getID() {
 		return id;
 	}
-
+	
+	public int getPlayCount() {
+		return playCount;
+	}
+	
+	public void setPlayCount(int count) {
+		this.playCount = count;
+	}
+	
+	public int getTimesCorrect() {
+		return timesCorrect;
+	}
+	
+	public void setTimesCorrect(int count) {
+		this.timesCorrect = count;
+	}
+	
+	public float getAvgGuessTime() {
+		return this.avgGuessTime;
+	}
+	
+	public void setAvgGuessTime(float f) {
+		this.avgGuessTime = f;
+	}
+	
+	public void playSong() {
+		this.setPlayCount(this.getPlayCount() + 1);
+	}
+	
+	public void guessCorrectly(int time) {
+		this.setAvgGuessTime((this.getAvgGuessTime() * this.getTimesCorrect() + time)/((float) (this.getTimesCorrect()+1)));
+		this.setTimesCorrect(this.getTimesCorrect() + 1);
+		
+	}
+	
 }
