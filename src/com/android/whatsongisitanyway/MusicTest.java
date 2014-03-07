202a6eef9e6d93524d53edca39c395375cd1aa2e
Annie Jiao <anniej@mit.edu>
2014-03-06 20:01:37 -0500

    tests for adts

--- src/com/android/whatsongisitanyway/MusicTest.java
+++ src/com/android/whatsongisitanyway/MusicTest.java
@@ -1,17 +1,38 @@
 package com.android.whatsongisitanyway;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class MusicTest {
 
-	Music song = new Music(42, "do i wanna know");
-
 	@Test
 	public void IDTest() {
+		Music song = new Music(42, "do i wanna know", "arctic monkeys", "180",
+				"rock");
 		assertEquals("do i wanna know", song.getID());
 
 	}
 
+	@Test
+	public void playCountTest() {
+		Music song = new Music(42, "do i wanna know", "arctic monkeys", "180",
+				"rock");
+		assertTrue(song.getPlayCount() == 0);
+		song.playSong();
+		song.playSong();
+		assertTrue(song.getPlayCount() == 2);
+
+		// times correct is initially 0
+		assertTrue(song.getTimesCorrect() == 0);
+		song.guessCorrectly(5);
+		assertTrue(song.getAvgGuessTime() == 5.0);
+		assertTrue(song.getTimesCorrect() == 1);
+
+		song.playSong();
+		assertTrue(song.getPlayCount() == 3);
+		song.guessCorrectly(2);
+		assertTrue(song.getAvgGuessTime() == 3.5);
+	}
 }
